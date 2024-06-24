c03f5d4da106bc9e579277a898627dde9c3a0f3b
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-24 12:19:15 +0700

    js lab 144

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// write code here
+// จง Group คนที่มีเพศเดียวกันเข้าด้วยกัน
+
+const persons = [
+  { name: "John", sex: "M" },
+  { name: "Jody", sex: "M" },
+  { name: "Susan", sex: "F" },
+  { name: "Kate", sex: "F" },
+  { name: "Sid", sex: "M" },
+];
+
+const group = persons.reduce((obj, person) => {
+  if (!obj[person.sex]) {
+    obj[person.sex] = [];
+  }
+  obj[person.sex].push(person.name);
+  return obj;
+}, {});
+console.log(group);
+
+// expexted result: { M: ['John', 'Jody', 'Sid'], F: ['Susan', 'Kate'] }
